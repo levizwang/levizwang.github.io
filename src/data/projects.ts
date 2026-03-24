f1efb841d9d87f4b9b65c8c0edf0714a83678793
levizwang <91720030+levizwang@users.noreply.github.com>
2026-03-24 17:16:04 +0800

    Remove sensitive data file

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,6 @@
 import mev1Image from '../images/projects/SOLANA_MEV.jpg';
+import agentImage from '../images/projects/Agent_eval.png';
+import ragImage from '../images/projects/RAG_eval.jpg';
 
 export interface Project {
   id: string;
@@ -15,5 +17,19 @@ export const projects: Project[] = [
     description: 'This project is a Solana MEV bot that reads the internet for you',
     image: mev1Image,
     link: 'https://github.com/levizwang/Solana-MEV',
+  },
+  {
+    id: '2',
+    name: 'Agent Evaluation Report',
+    description: 'Visualization report of the Agent evaluation project',
+    image: agentImage,
+    link: '/Agentkit.html',
+  },
+  {
+    id: '3',
+    name: 'RAG Evaluation Report',
+    description: 'Visualization report of the RAG evaluation project',
+    image: ragImage,
+    link: '/ragas.html',
   }
 ];
